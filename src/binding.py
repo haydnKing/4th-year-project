3a732ab861488eafcd5827b3be7796336b28615c
Haydn King <hjking734@gmail.com>
2013-03-30 20:57:23 +0000

    Put together basic report structure and formatting

--- src/binding.py
+++ src/binding.py
@@ -1,6 +1,7 @@
 """Attempt to match PPRs based on their binding preferences"""
 
-import extract, utils, ppr, PSSM
+import extract, utils, PSSM
+import ppr as PPR
 
 import matplotlib.pyplot as plt
 
@@ -35,8 +36,19 @@ def get_average(genome, genomes):
 def reduce_name(name):
 	return "".join([s[0].upper() for s in name.split('_')])
 
+def show_distance(num=0):
+	genomes = PPR.load_genomes()
+
+	ppr = genomes[0].pprs[num]
+	closest = minima(ppr, genomes)
+	left = range(len(closest[0]))
+
+	plt.bar(left, closest[1], width=1.0)
+	plt.xticks([l+0.5 for l in left], closest[0], rotation=90)
+	plt.show()
+
 def ppr_distance():
-	genomes = ppr.load_genomes()
+	genomes = PPR.load_genomes()
 
 	data = []
 
@@ -50,6 +62,49 @@ def ppr_distance():
 			data,
 			'output/Average_PPR_distance.dat')
 
+def get_groups(threshold=0.3):
+
+	stdout.write("\rLoading...")
+	stdout.flush()
+
+	genomes = PPR.load_genomes()
+
+	groups = []
+
+	total = sum([sum([1 for p in g.pprs]) for g in genomes])
+	done = 0
+	fmt = "\rGrouping ({{:{}d}}/{{}}) | {{:2.1f}}%, {{:.1f}} per group     ".format(
+			len(str(total)))
+	for genome in genomes:
+		for ppr in genome.pprs:
+			#find the best group
+			possible = []
+			#groups are possible if the distance to each member is lte threshold
+			for group in groups:
+				if max([ppr.distance(p2, 'MSE')[0] for p2 in group]) <= threshold:
+					possible.append(group)
+			#if there are no possible groups, start a new one
+			if not possible:
+				groups.append([ppr,])
+			#otherwise, append to the group with the lowest average distance
+			else:
+				avg = [sum([ppr.distance(p2, 'MSE')[0] for p2 in group])/float(len(group))
+									for group in possible]
+				possible[avg.index(min(avg))].append(ppr)
+			#Write progress info
+			done += 1
+			stdout.write(fmt.format(done,total, 100.0 * done / float(total),
+				done / float(len(groups))))
+			stdout.flush()
+
+	stdout.write("\r" + (" "*60) + "\r")
+	stdout.flush()
+	return groups
+
+def group_stats(groups):
+	l = [len(g) for g in groups]
+	print "{} groups, {} <= length <= {}, average = {}".format(
+			len(groups), min(l), max(l), sum(l)/float(len(l)))
 
 if __name__ == '__main__':
 	ppr_distance()
